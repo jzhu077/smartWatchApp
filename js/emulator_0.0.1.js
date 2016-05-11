ef428ef6f5dc3392e290dac413a586d3eacf6929
Frankyang88 <garinyang@gmail.com>
2016-05-11 22:05:59 +1200

    update_add_a simple analog clock
    swipte left in main screen is clock
    swipe right in main screen is google map

--- js/emulator_0.0.1.js
+++ js/emulator_0.0.1.js
@@ -3,45 +3,40 @@
  */
 $(document).ready(function() {
     var can, ctx, canX, canY, mouseIsDown = 0;
+    var can_clock,ctx_c,radius_c=0;
     var Swipe_Right = 0, Swipe_Left = 0, Swipe_Up = 0, Swipe_Down = 0;
     var lastMouseDown = {x: null, y: null};
     var map;
-
+    var Layer=0;
     $("body").init(function () {
         can = document.getElementById("myCanvas");
         ctx = can.getContext("2d");
-
         can.addEventListener("mousedown", mouseDown, false);
         can.addEventListener("mousemove", mouseXY, false);
         can.addEventListener("mouseup", mouseUp, false);
 
-    })
+
+        can_clock=document.getElementById("canvas_clock");
+        ctx_c=can_clock.getContext("2d");
+
+        radius_c = can_clock.height / 2;
+        ctx_c.translate(radius_c, radius_c);
+        radius_c = radius_c * 0.90;
+    });
 
 
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
 
+
     draw_watch_frame(ctx, 200, 200, 200, 200, 10);
     draw_inner_frame(ctx, 200, 200, 160, 160)
     google.maps.event.addDomListener(window, 'load', initialize);
-/*
-    var btn = document.createElement("BUTTON");        // Create a <button> element
-    var t = document.createTextNode("Quit");       // Create a text node
-    btn.id="quit"
-    btn.appendChild(t);                                // Append the text to <button>
-    document.body.appendChild(btn);
-    btn.style.position.relative;
-    btn.style.top=160;
-    btn.style.left=0;
-
-    document.getElementById("quit").addEventListener("click", quit);
-*/
 
 
-    //google.maps.event.addDomListener(button, 'click', quit);
 
     var imageObj = new Image();
-    imageObj.src='../images/apple.jpg';
+    imageObj.src='../images/travel.jpg';
     imageObj.id="apple";
     imageObj.style.zIndex=-2;
 
@@ -98,17 +93,27 @@ $(document).ready(function() {
             console.log("Mouse is up");
         if (Swipe_Right) {
             console.log("Swipe Right");
+            if (Layer == 0) {
             var maps = document.getElementById("googleMap");
             maps.style.zIndex = 0;
-
+            Layer=10;}
         }
-        if (Swipe_Left)
+        if (Swipe_Left){
             console.log("Swipe Left");
+            if(Layer==0){
+            runclock();
+            Layer=2;}
+        }
         if (Swipe_Up)
             console.log("Swipe Up");
         if (Swipe_Down)
+            if(Layer==2)
+            {
+                var clock = document.getElementById("canvas_clock");
+                clock.style.zIndex = -2;
+                Layer=0;
+            }
             console.log("Swipe Down");
-
     }
 
     function CenterControl(controlDiv, map) {
@@ -139,6 +144,7 @@ $(document).ready(function() {
         controlUI.addEventListener('click', function() {
             var maps = document.getElementById("googleMap");
             maps.style.zIndex = -10;
+            Layer=0;
         });
     }
 
@@ -191,6 +197,109 @@ $(document).ready(function() {
         ctx.quadraticCurveTo(x - width / 2, y - height / 2, x - width / 2 + edge, y - height / 2);
         ctx.stroke();
         ctx.fill();
+    }
+
+
+    /*
+     var btn = document.createElement("BUTTON");        // Create a <button> element
+     var t = document.createTextNode("Quit");       // Create a text node
+     btn.id="quit"
+     btn.appendChild(t);                                // Append the text to <button>
+     document.body.appendChild(btn);
+     btn.style.position.relative;
+     btn.style.top=160;
+     btn.style.left=0;
+
+     document.getElementById("quit").addEventListener("click", quit);
+     */
+
+    function runclock() {
+
+        can_clock.style.zIndex = 0;
+
+        can_clock.addEventListener("mousedown", mouseDown, false);
+        can_clock.addEventListener("mousemove", mouseXY, false);
+        can_clock.addEventListener("mouseup", mouseUp, false);
+
+        var run;
+
+        run=window.setInterval(drawClock,1000);
+        if(mouseIsDown)
+        window.clearInterval(run);
+
+
+    }
+    function drawClock() {
+        drawFace(ctx_c, radius_c);
+        drawNumbers(ctx_c, radius_c);
+        drawTime(ctx_c, radius_c);
+    }
+
+    function drawFace(ctxc, radius) {
+        var grad;
+        ctxc.beginPath();
+        ctxc.arc(0, 0, radius, 0, 2*Math.PI);
+        ctxc.fillStyle = 'white';
+        ctxc.fill();
+        grad = ctxc.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
+        grad.addColorStop(0, '#333');
+        grad.addColorStop(0.5, 'white');
+        grad.addColorStop(1, '#333');
+        ctxc.strokeStyle = grad;
+        ctxc.lineWidth = radius*0.1;
+        ctxc.stroke();
+        ctxc.beginPath();
+        ctxc.arc(0, 0, radius*0.1, 0, 2*Math.PI);
+        ctxc.fillStyle = '#333';
+        ctxc.fill();
+    }
+
+    function drawNumbers(ctx, radius) {
+        var ang;
+        var num;
+        ctx.font = radius*0.15 + "px arial";
+        ctx.textBaseline="middle";
+        ctx.textAlign="center";
+        for(num = 1; num < 13; num++){
+            ang = num * Math.PI / 6;
+            ctx.rotate(ang);
+            ctx.translate(0, -radius*0.85);
+            ctx.rotate(-ang);
+            ctx.fillText(num.toString(), 0, 0);
+            ctx.rotate(ang);
+            ctx.translate(0, radius*0.85);
+            ctx.rotate(-ang);
+        }
+    }
+
+    function drawTime(ctx, radius){
+        var now = new Date();
+        var hour = now.getHours();
+        var minute = now.getMinutes();
+        var second = now.getSeconds();
+        //hour
+        hour=hour%12;
+        hour=(hour*Math.PI/6)+
+        (minute*Math.PI/(6*60))+
+        (second*Math.PI/(360*60));
+        drawHand(ctx, hour, radius*0.5, radius*0.07);
+        //minute
+        minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+        drawHand(ctx, minute, radius*0.8, radius*0.07);
+        // second
+        second=(second*Math.PI/30);
+        drawHand(ctx, second, radius*0.9, radius*0.02);
+    }
+
+    function drawHand(ctx, pos, length, width) {
+        ctx.beginPath();
+        ctx.lineWidth = width;
+        ctx.lineCap = "round";
+        ctx.moveTo(0,0);
+        ctx.rotate(pos);
+        ctx.lineTo(0, -length);
+        ctx.stroke();
+        ctx.rotate(-pos);
     }
 
 })
