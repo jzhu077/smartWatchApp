a8f3f9a7822e4bd44a78a6a4a860e974585e2273
Frankyang88 <garinyang@gmail.com>
2016-07-29 10:14:18 +1200

    Update test.js

--- Prototype/testcase/test.js
+++ Prototype/testcase/test.js
@@ -1,11 +1,12 @@
-QUnit.test( "hello test", function(assert) {
-  assert.ok( 1 == "1", "Passed!" );
-});
+QUnit.test("set elementZindex", function( assert ) {
+		assert.equal( setelementZindex("element1",9),true);
+	});
 
-QUnit.test("Distance Test", function calcDistance(assert) {
-		var src = new google.maps.LatLng(-45.866815599999995,170.5178656);
-		var dst = new google.maps.LatLng(-45.8581207,170.51572880000003);
-		var x = (google.maps.geometry.spherical.computeDistanceBetween(dst, src) / 1000).toFixed(2);        
-        assert.ok(x == "0.98", "Passed");
-    });
+QUnit.test("set elementZindex", function(assert){
+		assert.equal( setelementZindex("element2",8),true);
+	});
+
+QUnit.test("set elementZindex", function(assert){
+		assert.equal( setelementZindex("element3",7),true);
+	});
 
