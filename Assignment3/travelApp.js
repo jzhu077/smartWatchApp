cb096df3dd5ae7d18698f7e0a260a2adcbabdfbd
Frankyang88 <garinyang@gmail.com>
2016-08-06 13:43:30 +1200

    Update travelApp.js

--- Assignment3/travelApp.js
+++ Assignment3/travelApp.js
@@ -4,7 +4,7 @@
 var app = (function () {
     "use strict";
     var keyword="";
-<<<<<<< HEAD
+
     var radius;
     
     var pub = {};
@@ -98,7 +98,7 @@ var app = (function () {
     }
     
 
-    function hastouchcoordinates (touchcoordinates){
+function hastouchcoordinates (touchcoordinates){
         
         var coordinates = emulator.coordinatesofEmulator(); 
         var width = emulator.width()-80;
@@ -141,81 +141,9 @@ var app = (function () {
                    && touchcoordinates.y>= 130+coordinates.y){
             radius=20000;
             showData(secondOptions());
-=======
-    var radius="";
-
-    var pub = {};
-
-    //To start the app with three main category selections.
-    //layout1 : the first button on left top, the following in horizontal center
-    // the emulator specifies the x and y position.
-    function start() {
-        var pageNum;
-        var coordinates = emulator.coordinatesofEmulator();
-        var menu = {
-            x:coordinates.x,
-            y:coordinates.y,
-            width:emulator.width(),
-            height:emulator.height(),
-            message1: "Accommodation",
-            message2: "Entertainment",
-            message3: "Public",
-            color: "#FFFFFF"
-        };
-        emulator.draw(menu.x + 7,menu.y+8, menu.width - 15, menu.height/4, menu.color);
-        emulator.draw(menu.x + 7,menu.y*1.7,menu.width - 15, menu.height/4, menu.color);
-        emulator.draw(menu.x + 7,menu.y*2.3,menu.width - 15, menu.height/4, menu.color);
-
-        emulator.write(menu.x + 7,menu.y+40,menu.message1);
-        emulator.write(menu.x + 7,menu.y*2,menu.message2);
-        emulator.write(menu.x + 7,menu.y*2.6,menu.message3);
-        if(emulator.getXY() != null) {
-            hastouchcoordinates();
         }
-
-        //secondPage();
-        pageNum=1;
     }
-
-    function hastouchcoordinates () {
-        var coordinates = emulator.coordinatesofEmulator();
-        var width = emulator.width()-15;
-        var height = emulator.height()/4;
-        if (pageNum===1){
-            if(emulator.touchcoordinates.y <= (height+coordinates.y)-20){
-                console.log("1 Accommodation was clicked");
-                if(keyword != ""){
-                    radius = "1km";
-                }else{
-                    keyword = "ac";
-                }
-                //secondPage();
-            }
-            else if(emulator.touchcoordinates.y <= (height+coordinates.y*2) -20){
-
-                console.log("2 Entertainment was clicked");
-                if(keyword != ""){
-                    radius = "5km";
-                }else{
-                    keyword = "en";
-                }
-                //secondPage();
-            }
-            else if(emulator.touchcoordinates.y <= (height+coordinates.y*2.6) -20){
-
-                console.log("3 Public was clicked");
-                if(keyword != ""){
-                    radius = "20km";
-                    console.log(keyword);
-                }else{
-                    keyword = "pu";
-                }
->>>>>>> origin/master
-            }
-        }
-<<<<<<< HEAD
-    }
-
+}
         //The second page shows the radius.
 function StartPage(){
         pageNum=0;
@@ -261,15 +189,6 @@ function StartPage(){
         pageNum=2;
         var coordinates = emulator.coordinatesofEmulator(); 
         var menu = {            
-=======
-
-    }
-
-    //The second page shows the radius.
-    function secondPage(){
-        var coordinates = emulator.coordinatesofEmulator();
-        var rad = {
->>>>>>> origin/master
             x:coordinates.x,
             y:coordinates.y,
             width:emulator.width(),
@@ -277,7 +196,6 @@ function StartPage(){
             message1: "1km",
             message2: "5km",
             message3: "20km",
-<<<<<<< HEAD
             color: "#FFFFFF"            
         };       
         emulator.clearScreen();
@@ -289,20 +207,6 @@ function StartPage(){
         emulator.write(menu.x + 40,menu.y+150,menu.message3);   
         
         
-        
-=======
-            color: "#FFFFFF"
-        };
-
-        emulator.draw(rad.x + 7,rad.y+8, rad.width - 15, rad.height/4, rad.color);
-        emulator.draw(rad.x + 7,rad.y*1.7,rad.width - 15, rad.height/4, rad.color);
-        emulator.draw(rad.x + 7,rad.y*2.3,rad.width - 15, rad.height/4, rad.color);
-
-        emulator.write(rad.x + 7,rad.y+40,rad.message1);
-        emulator.write(rad.x + 7,rad.y*2,rad.message2);
-        emulator.write(rad.x + 7,rad.y*2.6,rad.message3);
-        secondOptions();
->>>>>>> origin/master
     }
 
     function showData(data){
