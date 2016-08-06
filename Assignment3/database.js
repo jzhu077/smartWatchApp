30a188862e37534b87c85e31994215414fde979c
Frankyang88 <garinyang@gmail.com>
2016-08-06 15:00:18 +1200

    add database
    add data

---
+++ Assignment3/database.js
@@ -0,0 +1,118 @@
+var ac = [
+    {
+    name: "Dunedin & Academy Court Motel",
+    location: {lat: -45.8637714, long: 170.50957589999996},
+    address: ["624 George St","Dunedin","Dunedin 9016","New Zealand"]
+    },
+     {
+    name: "Alhambra Oaks Motor Lodge",
+    location: {lat:-45.8622211, long:170.511841},
+    address: ["588 Great King St","North Dunedin","Dunedin 9016","New Zealand"]
+    },
+     {
+    name: "Woodlands Motels and Apartments",
+    location: {lat:-45.8621471, long:170.5120061},
+    address: ["594 Great King St","North Dunedin","Dunedin 9016","New Zealand"]
+    },
+     {
+    name: "Commodore Motel",
+    location: {lat: -45.8581207, long:170.51572880000003},
+    address: ["932 Cumberland St","North Dunedin","Dunedin 9016","New Zealand"]
+    },
+     {
+    name: "858 George Street Motel",
+    location: {lat:-45.85955449999999, long: 170.51223089999996},
+    address: ["858 George St","North Dunedin","Dunedin 9016","New Zealand"]
+    },
+     {
+    name: "Bella Vista Motel Dunedin",
+    location: {lat:-45.8597867 , long:170.51321289999998},
+    address: ["858 George St","North Dunedin","Dunedin 9016","New Zealand"]
+    },
+     {
+    name: "Alexis Motor Lodge & Motels Dunedin",
+    location: {lat:-45.8665686, long:170.50772129999996},
+    address: ["475 George St","North Dunedin","Dunedin 9016","New Zealand"]
+    },
+     {
+    name: "Quest Dunedin",
+    location: {lat:-45.8728121, long:170.50732979999998},
+    address: ["333 Cumberland St","North Dunedin","Dunedin 9016","New Zealand"]
+    }   
+   
+];
+var pu = [
+{
+    name:"Entertainment Publications Dunedin",
+    location: {lat:-45.8728121, long:170.50732979999998},
+    address:["21 Dunbar St","Dunedin 9016", "New Zealand"]
+},
+{
+    name:"Video Tech",
+    location: {lat:-45.8728121, long:170.50732979999998},
+    address:["686 Great King St","Dunedin 9016", "New Zealand"]
+},
+{
+    name:"Dunedin City Library",
+    location: {lat:-45.8726941, long:170.5035676},
+    address:["230 Moray Pl","Dunedin 9016", "New Zealand"]
+},   
+{
+    name:"Physio Pool Dunedin",
+    location: {lat:-45.8705768, long:170.51019610000003},
+    address:["140 Hanover St","Dunedin 9016", "New Zealand"]
+},
+{
+    name:"Dunedin Public Art Gallery",
+    location: {lat:-45.8744863, long:170.50273889999994},
+    address:["30 The Octagon","Dunedin 9016", "New Zealand"]
+},
+  {
+    name:"Forsyth Barr Stadium",
+    location: {lat:-45.8728121, long:170.50732979999998},
+    address:["130 Anzac Ave","Dunedin 9016", "New Zealand"]
+},
+  {
+    name:"Otago Museum",
+    location: {lat:-45.8728121, long:170.50732979999998},
+    address:["419 Great King St","Dunedin 9016", "New Zealand"]
+},
+  {
+    name:"Dunedin i-SITE Visitor Information Centre",
+    location: {lat:-45.87368779999999, long:170.5037433},
+    address:["50 The Octagon","Dunedin 9016", "New Zealand"]
+}
+];
+
+var en = [
+{
+    name:"Iss Facility Services Dunedin",
+    location: {lat:-45.8697503, long:170.51408200000003},
+    address:["Ward St","Dunedin 9016", "New Zealand"]
+},
+{
+    name:"Full Spectrum Training Facility",
+    location: {lat:-45.8697503, long:170.51408200000003},
+    address:["541 Great King St","Dunedin 9016", "New Zealand"]
+},
+{
+    name:"Alibi Bar and Restaurant",
+    location: {lat:-45.8728121, long:170.50732979999998},
+    address:["1 Princes St","Dunedin 9016", "New Zealand"]
+},
+{
+    name:"Megazone Entertainment Center",
+    location: {lat:-45.8728121, long:170.50732979999998},
+    address:["1 Princes St","Dunedin 9016", "New Zealand"]
+},
+{
+    name:"Alibi Bar and Restaurant",
+    location: {lat:-45.8728121, long:170.50732979999998},
+    address:["170 Frederick Street","Dunedin 9016", "New Zealand"]
+},   
+{
+    name:"Starters Bar",
+    location: {lat:-45.8697503, long:170.51408200000003},
+    address:["157 Frederick St","Dunedin 9016", "New Zealand"]
+}
+];
