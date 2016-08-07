b115b0a9bce9d096fbe241424844a13a06acda90
Frankyang88 <garinyang@gmail.com>
2016-08-07 17:04:16 +1200

    update
    add comment

--- Assignment3/emulator.js
+++ Assignment3/emulator.js
@@ -13,8 +13,8 @@ var emulator = (function(){
   
   var pub = {};
 
-  pub.calcDistance = function(p1, p2) {
-      
+  //calculate distance between two points, require google(network) support    
+  pub.calcDistance = function(p1, p2) {      
     return (google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000).toFixed(2);
   };
     
@@ -46,7 +46,12 @@ pub.width = function(){
 pub.height = function(){
   return height;
 };
-
+pub.ctx=function(){
+    return ctx;   
+}
+pub.can=function(){
+    return can;
+}
  pub.draw = function(x,y,width, height, color){    
     ctx.fillStyle = color;  
     ctx.fillRect(x, y, width, height);
@@ -56,7 +61,6 @@ pub.height = function(){
   pub.write = function(x,y,message){
     ctx.font = 'italic 10pt Calibri';
     ctx.fillStyle='black';
-    //ctx.clearRect(x,y,width,height);
     ctx.fillText(message, x, y,120);
   }  
 
@@ -93,19 +97,91 @@ pub.height = function(){
 
   }
   
+    
+    //register mouse down event methods on canvas
+    //
     pub.addmousedownlistener=function(funct){
 
         can.addEventListener("mousedown", funct, false);
     }
 
+    //
+    //register mouseup event methods on canvas
     pub.addmouseuplistener=function(funct){
 
         can.addEventListener("mouseup", funct, false);
     }
+    
+    //register move event methods on canvas
     pub.addmousemovelistener=function(funct){
 
         can.addEventListener("mousemove", funct, false);
     }
+    
+    
+    pub.drawline=function(pos, length, width) { 
+        ctx.beginPath();
+        ctx.lineWidth = width;
+        ctx.lineCap = "round";
+        ctx.moveTo(0,0);
+        ctx.rotate(pos);
+        ctx.lineTo(0, -length);
+        ctx.stroke();
+        ctx.rotate(-pos);
+    }
+    
+    
+    //draw text:universal method
+    //
+    pub.fillTxt=function(string,x,y){      
+      ctx.font = string.font;
+      ctx.textBaseline=string.baseline;
+      ctx.textAlign=string.position;
+      ctx.fillStyle=string.style;
+      ctx.fillText(string.data, x, y,string.maxwidth);
+    }
+    
+    //rotateangle:rotate canvas 
+    //
+    pub.rotateangle=function(ang){
+        ctx.rotate(ang);
+    }
+    
+    
+    //transposition:
+    // translate the position to another coordinates
+    //
+    pub.transposition=function(x,y){
+        
+        ctx.translate(x,y);
+    }
+    
+    
+    
+    //savestate:save the current state of coordinate info
+    //
+    pub.savestate=function(){
+        ctx.save();
+    }
+    
+    //recoverstate
+    //recover the state saved before transposition
+    //
+    pub.recoverstate=function(){
+        
+        ctx.restore();
+    }
+    
+    
+    //draw a circle
+    //parameter:position,radius,color
+    pub.drawcircle=function(x,y,radius,color) {
+          ctx.beginPath();
+          ctx.arc(x, y, radius, 0, 2*Math.PI);
+          ctx.fillStyle = color;
+          ctx.fill();
+          ctx.stroke();
+    };
     
   pub.setup = function() {
     x = 101;
@@ -116,7 +192,7 @@ pub.height = function(){
         
         can = document.getElementById("emulator") ;
         ctx = can.getContext("2d");
-        console.log(can);
+
         draw_watch_frame(ctx, 200, 200, width, height, edge);
 
         draw_inner_frame(ctx, 200, 200, 160, 160);
