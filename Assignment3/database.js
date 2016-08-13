9134b25906b1c71d22390aa927133ba6b2442a53
adelatorre33 <afdbaltazar@hotmail.com>
2016-08-13 18:24:31 +1200

    added images to the database

--- Assignment3/database.js
+++ Assignment3/database.js
@@ -2,42 +2,60 @@ var ac = [
     {
     name: "Dunedin & Academy Court Motel",
     location: {lat: -45.8637714, long: 170.50957589999996},
-    address: ["624 George St","Dunedin","Dunedin 9016","New Zealand"]
+    address: ["624 George St","Dunedin","Dunedin 9016","New Zealand"],
+    images: "",
+    description: "'A 4-minute walk from natural science exhibits at the Otago Museum, "
     },
      {
     name: "Alhambra Oaks Motor Lodge",
     location: {lat:-45.8622211, long:170.511841},
-    address: ["588 Great King St","North Dunedin","Dunedin 9016","New Zealand"]
+    address: ["588 Great King St","North Dunedin","Dunedin 9016","New Zealand"],
+    images: "images/alhambra.jpg" ,
+    description: "Modest accomodation with kitchen facilities"
     },
      {
     name: "Woodlands Motels and Apartments",
     location: {lat:-45.8621471, long:170.5120061},
-    address: ["594 Great King St","North Dunedin","Dunedin 9016","New Zealand"]
+    address: ["594 Great King St","North Dunedin","Dunedin 9016","New Zealand"],
+    images: "images/woodlands.jpg",
+    description: "'Casual 1-2 bedroom motel rooms with kitchens, free laundry facilities and parking'"
     },
      {
     name: "Commodore Motel",
     location: {lat: -45.8581207, long:170.51572880000003},
-    address: ["932 Cumberland St","North Dunedin","Dunedin 9016","New Zealand"]
+    address: ["932 Cumberland St","North Dunedin","Dunedin 9016","New Zealand"],
+    images: "",
+    description: "Unfussy studios and apartments offering kitchen facilities in a low-key motel with free breakfast"
     },
      {
     name: "858 George Street Motel",
     location: {lat:-45.85955449999999, long: 170.51223089999996},
-    address: ["858 George St","North Dunedin","Dunedin 9016","New Zealand"]
+    address: ["858 George St","North Dunedin","Dunedin 9016","New Zealand"],
+    images: "",
+    description: "Simple motel with laid-back rooms & apartments featuring cooking facilities" +
+    "balconies or patios'"
     },
      {
     name: "Bella Vista Motel Dunedin",
     location: {lat:-45.8597867 , long:170.51321289999998},
-    address: ["858 George St","North Dunedin","Dunedin 9016","New Zealand"]
+    address: ["858 George St","North Dunedin","Dunedin 9016","New Zealand"],
+    images: "images/bella.jpg", 
+    description:"Unassuming rooms, some with kitchenettes, in a low-key motel with laundry and free parking"
     },
      {
     name: "Alexis Motor Lodge & Motels Dunedin",
     location: {lat:-45.8665686, long:170.50772129999996},
-    address: ["475 George St","North Dunedin","Dunedin 9016","New Zealand"]
+    address: ["475 George St","North Dunedin","Dunedin 9016","New Zealand"],
+    images:"",
+    description: "Unfussy studios & apartments at a relaxed city centre motel offering free breakfast."
     },
      {
     name: "Quest Dunedin",
     location: {lat:-45.8728121, long:170.50732979999998},
-    address: ["333 Cumberland St","North Dunedin","Dunedin 9016","New Zealand"]
+    address: ["333 Cumberland St","North Dunedin","Dunedin 9016","New Zealand"],
+    images: "",
+    description: "Unfussy studios & apartments in a straightforward hotel featuring free laundry facilities"
+
     }   
    
 ];
@@ -86,14 +104,24 @@ var pu = [
 
 var en = [
 {
-    name:"Iss Facility Services Dunedin",
-    location: {lat:-45.8697503, long:170.51408200000003},
-    address:["Ward St","Dunedin 9016", "New Zealand"]
-},
+        name:"McDonalds Dunedin",
+        location: {lat:-45.859907, long: 170.513291},
+        address:["711 Great King St","North Dunedin 9016", "New Zealand"]
+    },
+    {
+        name:"Domino's Pizza",
+        location: {lat:-45.859907, long:170.51408200000003},
+        address:["736 Great King St, North Dunedin","Dunedin 9016", "New Zealand"]
+    },
+    {
+        name:"KFC",
+        location: {lat:-45.85987 	, long:170.51327},
+        address:["714 Great King St","North Dunedin 9016", "New Zealand"]
+    },
 {
-    name:"Full Spectrum Training Facility",
-    location: {lat:-45.8697503, long:170.51408200000003},
-    address:["541 Great King St","Dunedin 9016", "New Zealand"]
+    name:"Korea House",
+    location: {lat:-45.86806, long:170.51483},
+    address:["450 George St","Dunedin 9016", "New Zealand"]
 },
 {
     name:"Alibi Bar and Restaurant",
@@ -101,9 +129,9 @@ var en = [
     address:["1 Princes St","Dunedin 9016", "New Zealand"]
 },
 {
-    name:"Megazone Entertainment Center",
+    name:"The Flying Squid",
     location: {lat:-45.8728121, long:170.50732979999998},
-    address:["1 Princes St","Dunedin 9016", "New Zealand"]
+    address:["118 Albany St","Dunedin 9016", "New Zealand"]
 },
 {
     name:"Alibi Bar and Restaurant",
@@ -111,8 +139,8 @@ var en = [
     address:["170 Frederick Street","Dunedin 9016", "New Zealand"]
 },   
 {
-    name:"Starters Bar",
+    name:"Emerson's Brewery",
     location: {lat:-45.8697503, long:170.51408200000003},
-    address:["157 Frederick St","Dunedin 9016", "New Zealand"]
+    address:["47 Anzac Avenue","Dunedin 9016", "New Zealand"]
 }
 ];
